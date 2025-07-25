2e07c7a5aaa54994d44eb49ae6131c558507d8b3
Allan Souza <allanronaldo95@gmail.com>
2025-07-25 00:26:58 -0300

    basic java project

---
+++ src/test/java/br/dev/allan/basic_java_project/BasicJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.allan.basic_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
